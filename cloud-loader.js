ce69227e2d5fc2bfd0d3cd2acb92d9de8b246864
Menes800 <r.menes89@gmail.com>
2026-08-03 21:47:56 +0200

    Reparer kategoriikon i 2.1-kjernen

--- cloud-loader.js
+++ cloud-loader.js
@@ -6,6 +6,7 @@
   const repairs = [
     ['email: cleanEmail(form.email.value)),', 'email: cleanEmail(form.email.value),'],
     ["toLocaleDateString('v'b-NO')", "toLocaleDateString('nb-NO')"],
+    ["icon: row.icon || '📦\", color:", "icon: row.icon || '📦', color:"],
   ];
 
   function repairSource(input) {
